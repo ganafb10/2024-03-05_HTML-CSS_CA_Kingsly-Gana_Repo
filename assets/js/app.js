c05f67e89683b1fb40aa60a5df10e12422097309
ganafb10 <ganafb10@gmail.com>
2024-03-05 11:20:26 +0100

    uploading files to GitHub

---
+++ assets/js/app.js
@@ -0,0 +1,46 @@
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain attribute:*/
+    file = elmnt.getAttribute("rainydays-html");
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
+          }
+          if (this.status == 404) {
+            elmnt.innerHTML = "Page not found.";
+          }
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("rainydays-html");
+          includeHTML();
+        }
+      };
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
+    }
+  }
+}
+
+(function () {
+  const mobileBtn = document.querySelector(".toggle-menu");
+  const menu = document.querySelector(".nav-left .menu-items");
+  console.log(mobileBtn, menu);
+  mobileBtn.addEventListener("click", () => {
+    menu.classList.toggle("open");
+  });
+})();
+
+// const toggleMenu = (e) => {
+//   console.log(e);
+// };
+
+includeHTML();
